ceccb46317e49880e66030c286dd1567f1441dba
edkrueger <edkrueger@gmail.com>
2020-10-19 12:53:58 -0500

    Blink many times.

--- morse.py
+++ morse.py
@@ -2,15 +2,19 @@ import RPi.GPIO as GPIO
 import time
 
 PIN = 5
-WAIT_DURATION = 1
+DURATION = 1
+MAX_BLINKS = 10
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(PIN, GPIO.OUT)
 
-GPIO.output(PIN, GPIO.HIGH)
-time.sleep(WAIT_DURATION)
-GPIO.output(PIN, GPIO.LOW)
-time.sleep(WAIT_DURATION)
+blinks = 0
+while blinks < MAX_BLINKS:
+     GPIO.output(PIN, GPIO.HIGH)
+     time.sleep(DURATION)
+     GPIO.output(PIN, GPIO.LOW)
+     time.sleep(DURATION)
+     blinks += 1
 
 
 
